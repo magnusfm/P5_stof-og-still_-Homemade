1c4ed64388c2bd36bc56f058949f633814a4595f
MagnusUcl <47404463+MagnusUcl@users.noreply.github.com>
2019-04-12 14:45:57 +0200

    startet på forsiden
    tilføjet lidt javascript på navbar

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
-function myFunction() {
-    alert("Hello! I am an alert box!");
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
   }
+  prevScrollpos = currentScrollPos;
+}
